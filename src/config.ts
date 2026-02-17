7a8ff5933c58b4f764b7a0a140ac4005f0d928f6
Jenny Hunter <jenny@researchlead.com>
2026-02-16 19:40:28 -0500

    Add firebase config details for hosting

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+// For Firebase Hosting: Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// For Firebase Hosting: Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
